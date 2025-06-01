d49aa24fde8f7180782c8159249d20d3179c99bd
Illia Bryka <tyky42212@gmail.com>
2025-06-01 14:43:26 +0200

    add database

---
+++ System obsługi osobowej komunikacji kolejowej/Program.cs
@@ -0,0 +1,17 @@
+namespace System_obsługi_osobowej_komunikacji_kolejowej
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new fon());
+        }
+    }
+}
